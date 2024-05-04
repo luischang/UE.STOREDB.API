4288393e0fa2a02d6a36193e1bf15a0bb4194517
Luis Chang <luis.chang@outlook.com>
2024-05-04 17:18:35 -0500

    Finish Crud Category Controller

--- UE.STOREDB.API/Controllers/CategoryController.cs
+++ UE.STOREDB.API/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UE.STOREDB.DOMAIN.Core.Entities;
 using UE.STOREDB.DOMAIN.Core.Interfaces;
 
 namespace UE.STOREDB.API.Controllers
@@ -23,7 +24,7 @@ namespace UE.STOREDB.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id) 
+        public async Task<IActionResult> GetById(int id)
         {
             var category = await _categoryRepository.GetById(id);
             if (category == null)
@@ -32,6 +33,28 @@ namespace UE.STOREDB.API.Controllers
             return Ok(category);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]Category category)
+        {
+            var result = await _categoryRepository.Insert(category);
+            if (!result) return BadRequest();
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody]Category category)
+        {
+            if(id != category.Id) return BadRequest();
+            var result = await _categoryRepository.Update(category);
+            if (!result) return BadRequest();
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _categoryRepository.Delete(id);
+            if (!result) return BadRequest();
+            return Ok(result);
+        }
 
     }
 }
