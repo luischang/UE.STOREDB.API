cf952d2f1bd4d51acbbe53455dd105872976439c
Luis Chang <luis.chang@outlook.com>
2024-05-04 16:54:06 -0500

    Added CategoryController

--- UE.STOREDB.API/Controllers/CategoryController.cs
+++ UE.STOREDB.API/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UE.STOREDB.DOMAIN.Core.Interfaces;
 
 namespace UE.STOREDB.API.Controllers
 {
@@ -7,6 +8,30 @@ namespace UE.STOREDB.API.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryRepository.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id) 
+        {
+            var category = await _categoryRepository.GetById(id);
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
 
     }
 }
--- UE.STOREDB.API/Program.cs
+++ UE.STOREDB.API/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using UE.STOREDB.DOMAIN.Core.Interfaces;
+using UE.STOREDB.DOMAIN.Infrastructure.Data;
+using UE.STOREDB.DOMAIN.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
+var _config = builder.Configuration;
+var cnx = _config.GetConnectionString("DevConnection");
+builder.Services
+    .AddDbContext<StoreDbueContext>
+    (options => options.UseSqlServer(cnx));
+
+builder.Services.AddTransient<ICategoryRepository,CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
